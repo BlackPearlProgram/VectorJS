651727c12f94609ad72cbcd0a930765f7102b320
BlackPearlProgram <121311056+BlackPearlProgram@users.noreply.github.com>
2023-03-19 13:12:46 +0100

    Better logic and usage
    You don't have normal functions but strings like this: vec["+"](0, 0, 1)

--- Vector.js
+++ Vector.js
@@ -1,30 +1,34 @@
-class Vector {
-    constructor(x, y, z) {
-        if(x != undefined) this.x = x;
-        if(y != undefined) this.y = y;
-        if(z != undefined) this.z = z;
-    }
-
-    add(vector) {
-        return new Vector(this.x + vector.x, this.y + vector.y, this.z + vector.z);
-    }
-
-    adde(vector) {
-        this = this.add(vector);
-        return this;
-    }
-
-    sub(vector) {
-
-    }
-
-    sube(vector) {
-
-    }
-
-    neg() {
-
-    }
+const Vector = function(x, y, z, w) {
+    if (x != undefined) this.x = x;
+    if (y != undefined) this.y = y;
+    if (z != undefined) this.z = z;
+    if (w != undefined) this.w = w;
 }
 
-// mabye function $(string) { action = string };
+Vector.prototype = {
+    "+": function(vector, y, z, w) {
+
+        let newVec = new Vector(vector, y, z, w);
+        if(typeof(vector) === "object") newVec = vector;
+
+        return new Vector(
+            (newVec.x == undefined | this.x == undefined) ? (this.x) : (this.x + newVec.x),
+            (newVec.y == undefined | this.y == undefined) ? (this.y) : (this.y + newVec.y),
+            (newVec.z == undefined | this.z == undefined) ? (this.z) : (this.z + newVec.z),
+            (newVec.w == undefined | this.w == undefined) ? (this.w) : (this.w + newVec.w)
+        );
+    },
+    
+    "+=": function(vector, y, z, w) {
+
+        let newVec = new Vector(vector, y, z, w);
+        if(typeof(vector) === "object") newVec = vector;
+
+        if(this.x != undefined) this.x = (vector.x == undefined) ? (this.x) : (this.x + vector.x);
+        if(this.y != undefined) this.y = (vector.y == undefined) ? (this.y) : (this.y + vector.y);
+        if(this.z != undefined) this.z = (vector.z == undefined) ? (this.z) : (this.z + vector.z);
+        if(this.w != undefined) this.w = (vector.w == undefined) ? (this.w) : (this.w + vector.w);
+
+        return this;
+    },
+}
